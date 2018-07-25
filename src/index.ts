ee8f5b3447067da8608448ca133a6e39ae3c0213
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-07-25 10:32:16 -0300

    index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Color,BaseColor} from './color';
+export { RGBAColor } from './rgb-color';
+export { HSVColor } from './hsv-color';
+export { Generator,ColorGenerator,RandomHSVColorGenerator} from './generator';
+export { ColorService } from './color.service';
